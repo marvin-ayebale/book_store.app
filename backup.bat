c1ce3ecacb0017e4c1e4bafbff18eb403bee0c50
egoproject <enockmci@gmail.com>
2024-05-02 19:30:43 +0300

    Update by: EGOproject on Thu 05/02/2024 19:30:43.83

--- backup.bat
+++ backup.bat
@@ -1,11 +1,29 @@
 @echo off
+echo
+echo
+echo THIS IS THE STATUS OF THE REPOSITORY
 git status
 pause
+CLS
+ECHO ADDING UPDATES
 git add .
 git add .
 pause
-git commit -m "Update by: EGOproject on %date%"
+CLS
+ECHO COMMITING UPDATES
+git commit -m "Update by: EGOproject on %date% %time%"
+pause
+CLS
+ECHO PULLING UPDATES MADE ROMOTELY ELSEWHERE
 git pull
+pause
+CLS
+ECHO PUSHING YOUR UPDATES TO REMOTE REPO
 git push
+pause
+CLS
+echo
+echo
+echo THIS IS THE STATUS OF THE REPOSITORY
 git status
 pause
