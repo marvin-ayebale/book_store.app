95746100fb992f44c45331f8f2b16771bb31893e
egoproject <enockmci@gmail.com>
2024-05-02 19:23:55 +0300

    Update by: EGOproject on Thu 05/02/2024

---
+++ template/footer.php
@@ -0,0 +1,19 @@
+      	<hr>
+
+      	<footer class="fixed-bottom bg-light bg-gradient border py-3 px-2">
+			<div class="container">
+				<div class="d-flex justify-content-between">
+					<div class="">
+						<p href="https://.....com" target="_blank" class="text-decoration-none text-muted fw-bold"> &copy; <?= date('Y') ?> Fresh Pages Book Stores Site </p>
+					</div>
+					<div class="">
+						<a href="admin.php" class="text-decoration-none text-dark fw-bolder">Login as Admin</a>
+					</div>
+				</div>
+			</div>
+      	</footer>
+		<div class="clear-fix py-4"></div>
+    </div> <!-- /container -->
+
+  </body>
+</html>
